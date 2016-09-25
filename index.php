3d617bdab90ca32f4d25091b57050843f14232af
amardeepk <amardeepkumar15091996@gmail.com>
2016-09-25 23:55:38 +0530

    shifted to github
    hostel web app

---
+++ index.php
@@ -0,0 +1,211 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="NSIT’s hostel provide a wide range of facilities to ensure that students enjoy a vibrant campus life, while simultaneously accommodating a large number of students. A total of four hostel buildings (BH1, BH2, BH3, BH4) are present for male students, one for each year. There are two girls’ hostels (GH). Each hostel is well maintained with staff working overtime to ensure that students live comfortably inside the building premises. The hostel buildings are located close to the canteens as well as the administrative block (where classes ensue). Mess food is nutritious with a different menu for each day of the week.">
+    <meta name="author" content="bh2nsit NSIT">
+    <link rel="shortcut icon" href="assets/ico/favicon.png">
+
+    <title>bh2nsit</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="assets/css/main.css" rel="stylesheet">
+
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <!-- Fixed navbar -->
+    <div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.html"><img class="logo"src="assets/img/logo.jpg"></a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li class="active"><a href="index.html">HOME</a></li>
+            <li><a href="notice.html">NOTICE</a></li>
+            <li><a href="administration.html">ADMINISTRATION</a></li>
+            <li><a href="committee.html">COMMITTEE</a></li>
+            <li><a href="rules.html">RULES</a></li>
+            
+            <li><a data-toggle="modal" data-target="#myModal" href="#myModal"><i class="fa fa-envelope-o"></i></a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+
+	<div id="headerwrap">
+		<div class="container">
+			<div class="row centered">
+				<div >
+				<h1><b>BOYS HOSTEL-2</b></h1>
+				<a href="http://www.nsit.ac.in"><h2>Netaji Subhas Institute Of Technology</h2></a>
+				<ul class="admin">
+					<li class="1"><div class="2"><img class="aa"src="assets/img/director.jpg"><h3><b>DIRECTOR</b></h3>
+						<h3>Prof. Yogesh Singh</h3>
+
+					</div></li>
+					<li class="1"><div class="2"><img class="aa"src="assets/img/dsw.jpg"><h3><b>DSW</b></h3>
+						<h3>DR. M.P.S Bhatia</h3>
+					</div></li>
+					<li class="1"><div class="2"><img class="aa"src="assets/img/warden.jpg"><h3><b>WARDEN(BH-2)</b></h3>
+						<h3>Kunwar Singh</h3>
+					</div></li>
+					<li class="1"><div class="2"><img class="aa"src="assets/img/care.jpg"><h3><b>CARETAKER(BH-2)</b></h3>
+						<h3>Anoop Kumar Singh</h3>
+					</div></li>
+					
+				</ul>	
+				</div>
+			</div><!-- row -->
+		</div><!-- container -->
+	</div><!-- headerwrap -->
+
+
+	
+
+	<!-- PORTFOLIO SECTION -->
+	<div id="dg">
+		<div class="container">
+			<div class="row centered">
+				<h2><b>ABOUT US</b></h2>
+				<br>
+				<p class="parah">NSIT’s hostel provide a wide range of facilities to ensure that students enjoy a vibrant campus life, while simultaneously accommodating a large number of students. A total of four hostel buildings (BH1, BH2, BH3, BH4) are present for male students, one for each year. There are two girls’ hostels (GH). Each hostel is well maintained with staff working overtime to ensure that students live comfortably inside the building premises. The hostel buildings are located close to the canteens as well as the administrative block (where classes ensue). Mess food is nutritious with a different menu for each day of the week.
+Additionally, each boys’ hostel has the capacity to accommodate up to 276 students in single & double-bedded rooms. Students are allotted rooms on the basis of their permanent addresses & sorted distance wise. The outside Delhi candidates are thus given top priority. However, most students who apply for a hostel room are able to get one as soon as possible.Each hostel has its own in-premises mess.
+The hostels have 24 hours water supply & power backup available. A parking lot is located outside each hostel for the convenience of students.
+</p>
+			</div><!-- row -->
+		</div><!-- container -->
+	</div><!-- DG -->
+
+<style type="text/css">
+body {
+	background: #222;
+	color: #eee;
+	margin-top: 20px;
+	font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
+}
+a {
+	color: #FFF;
+}
+a:hover {
+	color: yellow;
+	text-decoration: underline;
+}
+.thumbnails img {
+	height: 80px;
+	border: 4px solid #555;
+	padding: 1px;
+	margin: 0 10px 10px 0;
+}
+
+.thumbnails img:hover {
+	border: 4px solid #00ccff;
+	cursor:pointer;
+}
+
+.preview img {
+	border: 4px solid #444;
+	padding: 1px;
+	width: 800px;
+}
+</style>
+<div class="gallery" align="center">
+	<h1><b>Photo Gallery</b></h1>
+	
+
+	<div class="thumbnails">
+		<img onmouseover="preview.src=img1.src" name="img1" src="images/img1.jpg" alt=""/>
+		<img onmouseover="preview.src=img2.src" name="img2" src="images/img2.jpg" alt=""/>
+		<img onmouseover="preview.src=img3.src" name="img3" src="images/img3.jpg" alt=""/>
+		<img onmouseover="preview.src=img4.src" name="img4" src="images/img4.jpg" alt=""/>
+		<img onmouseover="preview.src=img5.src" name="img5" src="images/img5.jpg" alt=""/>
+		<img onmouseover="preview.src=img6.src" name="img6" src="images/img6.jpg" alt=""/>
+	</div><br/>
+
+	<div class="preview" align="center">
+		<img name="preview" src="images/img1.jpg" alt=""/>
+	</div>
+<br>
+</div>
+
+
+	<div id="r">
+		<div class="container">
+			<div class="row centered">
+				<div class="col-lg-8 col-lg-offset-2">
+					<h3>NSIT</h3>
+					<p></p>
+				</div>
+			</div><!-- row -->
+		</div><!-- container -->
+	</div><! -- r wrap -->
+	
+	
+	<!-- FOOTER -->
+	<div id="f">
+		<div class="container">
+			<div class="row centered">
+				<a href="https://www.facebook.com/boyshostelnsit/?fref=ts&ref=br_tf"><i class="fa fa-facebook"></i></a>
+		
+			</div><!-- row -->
+		</div><!-- container -->
+	</div><!-- Footer -->
+
+
+	<!-- MODAL FOR CONTACT -->
+	<!-- Modal -->
+	<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+	  <div class="modal-dialog">
+	    <div class="modal-content">
+	      <div class="modal-header">
+	        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+	        <h4 class="modal-title" id="myModalLabel">contact us</h4>
+	      </div>
+	      <div class="modal-body">
+		        <div class="row centered">
+		        	<p>We are available 24/7, so don't hesitate to contact us.</p>
+		        	<p>
+		        		Boys Hostel<br/>
+						NSIT College<br/>
+						Dwarka<br/>
+						Delhi-110078
+		        	</p>
+		        	<div id="mapwrap">
+		<iframe height="300" width="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.es/maps?t=m&amp;ie=UTF8&amp;ll=28.6123527,77.0345874&amp;spn=67.34552,156.972656&amp;z=16&amp;output=embed"></iframe>
+					</div>	
+		        </div>
+	      </div>
+	      <div class="modal-footer">
+	        <button type="button" class="btn btn-danger" data-dismiss="modal">Exit</button>
+	      </div>
+	    </div><!-- /.modal-content -->
+	  </div><!-- /.modal-dialog -->
+	</div><!-- /.modal -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+  </body>
+</html>
